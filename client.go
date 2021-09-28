ea252b9d94ceaff2b05648e1cb298d6bb1c7f9f1
mrshukla1805 <nageshshukla1805@gmail.com>
2021-09-28 10:31:43 +0530

    simple-rmq-communication

---
+++ client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	connector "github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := connector.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to connect to RabbitMq", err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open channel", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"first", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	body := "Nagesh Shukla!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		connector.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to publish message", err)
+	}
+	log.Printf(" [x] Sent %s", body)
+}
---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	connector "github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := connector.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to Connect to Rabbit MQ", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"first", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to register a consumer", err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+}
